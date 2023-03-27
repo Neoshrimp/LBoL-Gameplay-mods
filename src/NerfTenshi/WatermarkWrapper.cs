8ad8bddd822ae3668ec1f83b42997ba5a0ac59fc
Neoshrimp <shadow638@protonmail.com>
2023-03-27 17:23:34 +0300

    Add NoInlining attribute

--- src/NerfTenshi/WatermarkWrapper.cs
+++ src/NerfTenshi/WatermarkWrapper.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace NerfTenshi
 {
     internal class WatermarkWrapper
     {
+        [MethodImpl(MethodImplOptions.NoInlining)]
         internal static void ActivateWatermark() => AddWatermark.API.ActivateWatermark();
     }
 }
