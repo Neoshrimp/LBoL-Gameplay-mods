f98acc4fd3eafe45802c0b347535b684644b9d66
Neoshrimp <shadow638@protonmail.com>
2023-04-23 01:36:32 +0300

    BanList error feedback

--- src/BanList/Plugin.cs
+++ src/BanList/Plugin.cs
@@ -1,124 +1,15 @@
 ﻿using BepInEx;
-using BepInEx.Configuration;
 using HarmonyLib;
-using LBoL.Base;
-using LBoL.Base.Extensions;
 using LBoL.ConfigData;
-using LBoL.Core;
-using LBoL.Core.Adventures;
-using LBoL.Core.Attributes;
-using LBoL.Core.Battle;
-using LBoL.Core.Battle.BattleActionRecord;
-using LBoL.Core.Battle.BattleActions;
-using LBoL.Core.Battle.Interactions;
-using LBoL.Core.Cards;
-using LBoL.Core.Dialogs;
-using LBoL.Core.GapOptions;
-using LBoL.Core.Helpers;
-using LBoL.Core.Intentions;
-using LBoL.Core.JadeBoxes;
-using LBoL.Core.PlatformHandlers;
-using LBoL.Core.Randoms;
-using LBoL.Core.SaveData;
-using LBoL.Core.Stations;
-using LBoL.Core.Stats;
-using LBoL.Core.StatusEffects;
-using LBoL.Core.Units;
-using LBoL.EntityLib.Adventures;
-using LBoL.EntityLib.Adventures.Common;
-using LBoL.EntityLib.Adventures.FirstPlace;
-using LBoL.EntityLib.Adventures.Shared12;
-using LBoL.EntityLib.Adventures.Shared23;
-using LBoL.EntityLib.Adventures.Stage1;
-using LBoL.EntityLib.Adventures.Stage2;
-using LBoL.EntityLib.Adventures.Stage3;
-using LBoL.EntityLib.Cards.Character.Cirno;
-using LBoL.EntityLib.Cards.Character.Cirno.FairySupport;
-using LBoL.EntityLib.Cards.Character.Koishi;
-using LBoL.EntityLib.Cards.Character.Marisa;
-using LBoL.EntityLib.Cards.Character.Reimu;
-using LBoL.EntityLib.Cards.Character.Sakuya;
-using LBoL.EntityLib.Cards.Devel;
-using LBoL.EntityLib.Cards.Neutral;
-using LBoL.EntityLib.Cards.Neutral.Black;
-using LBoL.EntityLib.Cards.Neutral.Blue;
-using LBoL.EntityLib.Cards.Neutral.Green;
-using LBoL.EntityLib.Cards.Neutral.MultiColor;
-using LBoL.EntityLib.Cards.Neutral.NoColor;
-using LBoL.EntityLib.Cards.Neutral.Red;
-using LBoL.EntityLib.Cards.Neutral.TwoColor;
-using LBoL.EntityLib.Cards.Neutral.White;
-using LBoL.EntityLib.Cards.Other.Adventure;
-using LBoL.EntityLib.Cards.Other.Enemy;
-using LBoL.EntityLib.Cards.Other.Misfortune;
-using LBoL.EntityLib.Cards.Other.Tool;
-using LBoL.EntityLib.Devel;
-using LBoL.EntityLib.Dolls;
-using LBoL.EntityLib.EnemyUnits.Character;
-using LBoL.EntityLib.EnemyUnits.Character.DreamServants;
-using LBoL.EntityLib.EnemyUnits.Lore;
-using LBoL.EntityLib.EnemyUnits.Normal;
-using LBoL.EntityLib.EnemyUnits.Normal.Bats;
-using LBoL.EntityLib.EnemyUnits.Normal.Drones;
-using LBoL.EntityLib.EnemyUnits.Normal.Guihuos;
-using LBoL.EntityLib.EnemyUnits.Normal.Maoyus;
-using LBoL.EntityLib.EnemyUnits.Normal.Ravens;
-using LBoL.EntityLib.EnemyUnits.Opponent;
-using LBoL.EntityLib.Exhibits;
-using LBoL.EntityLib.Exhibits.Adventure;
-using LBoL.EntityLib.Exhibits.Common;
-using LBoL.EntityLib.Exhibits.Mythic;
-using LBoL.EntityLib.Exhibits.Seija;
-using LBoL.EntityLib.Exhibits.Shining;
-using LBoL.EntityLib.JadeBoxes;
-using LBoL.EntityLib.Mixins;
-using LBoL.EntityLib.PlayerUnits;
-using LBoL.EntityLib.Stages;
-using LBoL.EntityLib.Stages.NormalStages;
-using LBoL.EntityLib.StatusEffects.Basic;
-using LBoL.EntityLib.StatusEffects.Cirno;
-using LBoL.EntityLib.StatusEffects.Enemy;
-using LBoL.EntityLib.StatusEffects.Enemy.SeijaItems;
-using LBoL.EntityLib.StatusEffects.Marisa;
-using LBoL.EntityLib.StatusEffects.Neutral;
-using LBoL.EntityLib.StatusEffects.Neutral.Black;
-using LBoL.EntityLib.StatusEffects.Neutral.Blue;
-using LBoL.EntityLib.StatusEffects.Neutral.Green;
-using LBoL.EntityLib.StatusEffects.Neutral.Red;
-using LBoL.EntityLib.StatusEffects.Neutral.TwoColor;
-using LBoL.EntityLib.StatusEffects.Neutral.White;
-using LBoL.EntityLib.StatusEffects.Others;
-using LBoL.EntityLib.StatusEffects.Reimu;
-using LBoL.EntityLib.StatusEffects.Sakuya;
-using LBoL.EntityLib.UltimateSkills;
 using LBoL.Presentation;
-using LBoL.Presentation.Animations;
-using LBoL.Presentation.Bullet;
-using LBoL.Presentation.Effect;
-using LBoL.Presentation.I10N;
-using LBoL.Presentation.UI;
-using LBoL.Presentation.UI.Dialogs;
-using LBoL.Presentation.UI.ExtraWidgets;
 using LBoL.Presentation.UI.Panels;
-using LBoL.Presentation.UI.Transitions;
-using LBoL.Presentation.UI.Widgets;
-using LBoL.Presentation.Units;
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Reflection.Emit;
-using System.Runtime.CompilerServices;
-using System.Threading.Tasks;
 using UnityEngine;
-using Untitled;
-using Untitled.ConfigDataBuilder;
-using Untitled.ConfigDataBuilder.Base;
-using YamlDotNet.RepresentationModel;
 using YamlDotNet.Serialization;
-using Debug = UnityEngine.Debug;
+using TMPro;
+
 
 
 namespace BanList
@@ -159,10 +50,18 @@ namespace BanList
         }
 
 
+        enum FailureLevel 
+        {
+            None,
+            Warning,
+            Critical,
+        }
+
         static List<string> banList = new List<string>();
 
         static List<string> upgradeRestrictList = new List<string>();
 
+        static FailureLevel failureLevel = FailureLevel.None;
 
 
         static void LoadAndParseYaml()
@@ -191,12 +90,14 @@ namespace BanList
                 catch (Exception ex)
                 {
 
+                    failureLevel = FailureLevel.Critical;
                     log.LogError($"Error while parsing yaml. Check if the yaml format is valid.");
                     log.LogError($"{ex}");
                 }
             }
             catch (Exception ex)
             {
+                failureLevel = FailureLevel.Critical;
                 log.LogError("Error while loading BanList.yaml. Are you sure BanList.yaml is in the same directory as BanList.dll?");
                 log.LogError(ex);
             }
@@ -222,6 +123,7 @@ namespace BanList
                     }
                     else
                     {
+                        failureLevel = FailureLevel.Warning;
                         log.LogWarning($"Banlist: card with id: {id} was not found.");
                     }
                 }
@@ -235,12 +137,41 @@ namespace BanList
                     }
                     else
                     {
-                        log.LogWarning($"Restrict list: card with id: {id} was not found.");
+                        failureLevel = FailureLevel.Warning;
+                        log.LogWarning($"Upgrade restrict list: card with id: {id} was not found.");
                     }
                 }
             }
         }
 
+
+        // 2do make better/working ui feedback
+        // [HarmonyPatch(typeof(StartGamePanel), nameof(StartGamePanel.OnShowing))]
+        class StartGamePanel_Patch
+        {
+            static void Postfix(StartGamePanel __instance)
+            {
+                var hintText = __instance.difficultyPanelRoot.transform.Find("DifficultyHint")?.GetComponent<TextMeshProUGUI>();
+                if (hintText != null)
+                {
+                    switch (failureLevel)
+                    {
+                        case FailureLevel.None:
+                            hintText.text = "Banlist mode active!";
+                            break;
+                        case FailureLevel.Warning:
+                            hintText.text = "Banlist mode active, but some card ids were wrong. Check log at BepInEx/LogOutput.log for details";
+
+                            break;
+                        case FailureLevel.Critical:
+                            hintText.text = "Banlist mode  Check log at BepInEx/LogOutput.log for details";
+                            break;
+                    }
+                }
+
+
+            }
+        }
 
 
 
