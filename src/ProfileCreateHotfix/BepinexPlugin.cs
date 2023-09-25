723df22dbd76e17df74bcd6beb816030ad14df1e
Neoshrimp <shadow638@protonmail.com>
2023-09-26 02:09:35 +0300

    add hotfix mod

---
+++ src/ProfileCreateHotfix/BepinexPlugin.cs
@@ -0,0 +1,65 @@
+﻿using BepInEx;
+using HarmonyLib;
+using LBoL.Presentation;
+using LBoL.Presentation.UI.Panels;
+using UnityEngine;
+
+
+namespace ProfileCreateHotfix
+{
+    [BepInPlugin(ProfileCreateHotfix.PInfo.GUID, ProfileCreateHotfix.PInfo.Name, ProfileCreateHotfix.PInfo.version)]
+
+    [BepInProcess("LBoL.exe")]
+    public class BepinexPlugin : BaseUnityPlugin
+    {
+
+        private static readonly Harmony harmony = ProfileCreateHotfix.PInfo.harmony;
+
+        internal static BepInEx.Logging.ManualLogSource log;
+
+
+
+
+        private void Awake()
+        {
+            log = Logger;
+
+            // very important. Without this the entry point MonoBehaviour gets destroyed
+            DontDestroyOnLoad(gameObject);
+            gameObject.hideFlags = HideFlags.HideAndDontSave;
+
+
+            harmony.PatchAll();
+
+        }
+
+        private void OnDestroy()
+        {
+            if (harmony != null)
+                harmony.UnpatchSelf();
+        }
+
+
+
+
+
+
+
+        [HarmonyPatch(typeof(StartupSelectLocalePanel), nameof(StartupSelectLocalePanel.Awake))]
+        class StartupSelectLocalePanel_Patch
+        {
+            static void Prefix()
+            {
+                var ge = GameObject.Find("GameEntry");
+                ge.GetComponent<GameEntry>().alioth.gameObject.SetActive(false);
+                ge.GetComponent<GameEntry>().shanghaiAlice.gameObject.SetActive(false);
+
+            }
+
+        }
+
+
+
+
+    }
+}
---
+++ src/ProfileCreateHotfix/PInfo.cs
@@ -0,0 +1,14 @@
+﻿using HarmonyLib;
+
+namespace ProfileCreateHotfix
+{
+    public static class PInfo
+    {
+        // each loaded plugin needs to have a unique GUID. usually author+generalCategory+Name is good enough
+        public const string GUID = "neo.lbol.fixes.1.3.15splashScreen";
+        public const string Name = "Intro screen fix";
+        public const string version = "1.0.0";
+        public static readonly Harmony harmony = new Harmony(GUID);
+
+    }
+}
